07e973cdba44a5c66f91224f5acae4bde05cea34
Boyd Thompson <Wyvar@Boyds-iMac-2.local>
2014-10-01 18:13:09 -0700

    added a test class

---
+++ BTTest.h
@@ -0,0 +1,13 @@
+//
+//  BTTest.h
+//  Git Test
+//
+//  Created by Boyd Thompson on 10/1/14.
+//  Copyright (c) 2014 WyvarKahn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BTTest : NSObject
+
+@end
---
+++ BTTest.m
@@ -0,0 +1,13 @@
+//
+//  BTTest.m
+//  Git Test
+//
+//  Created by Boyd Thompson on 10/1/14.
+//  Copyright (c) 2014 WyvarKahn. All rights reserved.
+//
+
+#import "BTTest.h"
+
+@implementation BTTest
+
+@end
